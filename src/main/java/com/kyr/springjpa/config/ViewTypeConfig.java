cda5c421d14088412db77d6dbe78b8156617f53e
yeongroke <chfaos120@naver.com>
2021-07-30 23:34:40 +0900

    [FEA] 데이터 타입에 따라 데이터 PATH 변환 로직 추가

--- src/main/java/com/kyr/springjpa/config/ViewTypeConfig.java
+++ src/main/java/com/kyr/springjpa/config/ViewTypeConfig.java
@@ -1,6 +1,22 @@
 package com.kyr.springjpa.config;
 
 
-public class ViewTypeConfig {
+import com.kyr.springjpa.util.Constants;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.File;
+
+@Slf4j
+public class ViewTypeConfig {
+    protected String typeReturnConfig(String type){
+        File file = new File("src/main/resource");
+        String returnConfigPath = file.getAbsolutePath();
+        switch(type){
+            case "test1" :
+                returnConfigPath += Constants.file2;
+                break;
+        }
+        log.info("viewtypeconfig-> " + returnConfigPath);
+        return returnConfigPath;
+    }
 }
