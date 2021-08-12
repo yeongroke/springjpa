2fac176cfb8cfa552d66971940b37b83f4553a0f
yeongroke <chfaos120@naver.com>
2021-08-12 23:11:11 +0900

    [REF] 필터 로그 정보 명확하게 변경

--- src/main/java/com/kyr/springjpa/filter/WebConfigFilter.java
+++ src/main/java/com/kyr/springjpa/filter/WebConfigFilter.java
@@ -1,4 +1,4 @@
-package com.kyr.springjpa.config;
+package com.kyr.springjpa.filter;
 
 import java.io.IOException;
 
@@ -24,17 +24,22 @@ public class WebConfigFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
             throws IOException, ServletException {
-        log.info("do! Filter -> ");
 
-        HttpServletResponse httpResp = (HttpServletResponse) res;
+        log.info("do! Filter Encoding -> {} , ContentType -> {} , ContextPath -> {} , "
+                , servletRequest.getCharacterEncoding()
+                , servletRequest.getContentType()
+                , servletRequest.getServletContext().getContextPath());
+
+
+        HttpServletResponse httpResp = (HttpServletResponse) servletResponse;
 
         httpResp.setHeader("Access-Control-Allow-Origin", "*"); //허용대상 도메인
         httpResp.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");
         httpResp.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept");
 
-        chain.doFilter(req, res);
+        chain.doFilter(servletRequest, servletResponse);
     }
 }
 
